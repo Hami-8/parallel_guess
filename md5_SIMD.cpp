ad3993773c499f64bb0a30f65fac335f94beaf23
Hami-8 <1809493351@qq.com>
2025-04-16 11:43:27 +0800

    MD5Hash函数的SIMD

--- md5_SIMD.cpp
+++ md5_SIMD.cpp
@@ -1,4 +1,4 @@
-#include "md5.h"
+#include "md5_SIMD.h"
 #include <iomanip>
 #include <assert.h>
 #include <chrono>
@@ -80,128 +80,152 @@ Byte *StringProcess(string input, int *n_byte)
  * @param[out] state 用于给调用者传递额外的返回值，即最终的缓冲区，也就是MD5的结果
  * @return Byte消息数组
  */
-void MD5Hash(string input, bit32 *state)
+void MD5Hash(const std::string inputs[4], bit32 state_out[4][4])
 {
 
-	Byte *paddedMessage;
-	int *messageLength = new int[1];
-	for (int i = 0; i < 1; i += 1)
+	Byte* paddedMessages[4];
+	int paddedLengths[4];
+	for (int i = 0; i < 4; i += 1)
 	{
-		paddedMessage = StringProcess(input, &messageLength[i]);
+		paddedMessages[i] = StringProcess(inputs[i], &paddedLengths[i]);
 		// cout<<messageLength[i]<<endl;
-		assert(messageLength[i] == messageLength[0]);
+		// assert(messageLength[i] == messageLength[0]);
 	}
-	int n_blocks = messageLength[0] / 64;
+	int paddedLength = paddedLengths[0];
+    int n_blocks = paddedLength / 64;  // 每个块 64 字节
 
 	// bit32* state= new bit32[4];
-	state[0] = 0x67452301;
-	state[1] = 0xefcdab89;
-	state[2] = 0x98badcfe;
-	state[3] = 0x10325476;
+	// 初始化 MD5 状态，每个状态都用 NEON 向量存储 4 个相同初始值（对应 4 个消息）
+	uint32x4_t A = vdupq_n_u32(0x67452301);
+	uint32x4_t B = vdupq_n_u32(0xefcdab89);
+	uint32x4_t C = vdupq_n_u32(0x98badcfe);
+	uint32x4_t D = vdupq_n_u32(0x10325476);
 
-	// 逐block地更新state
-	for (int i = 0; i < n_blocks; i += 1)
+	// 对于每个 512-bit 块同时处理 4 个口令
+	for (int block = 0; block < n_blocks; block++)
 	{
-		bit32 x[16];
+		// 为当前块构造 16 个 32 位字的向量 x[0..15]
+        uint32x4_t x[16];
 
 		// 下面的处理，在理解上较为复杂
 		for (int i1 = 0; i1 < 16; ++i1)
 		{
-			x[i1] = (paddedMessage[4 * i1 + i * 64]) |
-					(paddedMessage[4 * i1 + 1 + i * 64] << 8) |
-					(paddedMessage[4 * i1 + 2 + i * 64] << 16) |
-					(paddedMessage[4 * i1 + 3 + i * 64] << 24);
+			uint32_t word[4];  // 存储 4 个消息对应位置的 32 位字
+			for (int j = 0; j < 4; j++) {
+                int idx = block * 64 + i1 * 4;
+                word[j] = paddedMessages[j][idx] |
+                          (paddedMessages[j][idx+1] << 8) |
+                          (paddedMessages[j][idx+2] << 16) |
+                          (paddedMessages[j][idx+3] << 24);
+            }
+			x[i1] = vld1q_u32(word);  // 加载为 NEON 向量
 		}
 
-		bit32 a = state[0], b = state[1], c = state[2], d = state[3];
+		// 保存当前状态 for this block
+        uint32x4_t a = A;
+        uint32x4_t b = B;
+        uint32x4_t c = C;
+        uint32x4_t d = D;
 
 		auto start = system_clock::now();
 		/* Round 1 */
-		FF(a, b, c, d, x[0], s11, 0xd76aa478);
-		FF(d, a, b, c, x[1], s12, 0xe8c7b756);
-		FF(c, d, a, b, x[2], s13, 0x242070db);
-		FF(b, c, d, a, x[3], s14, 0xc1bdceee);
-		FF(a, b, c, d, x[4], s11, 0xf57c0faf);
-		FF(d, a, b, c, x[5], s12, 0x4787c62a);
-		FF(c, d, a, b, x[6], s13, 0xa8304613);
-		FF(b, c, d, a, x[7], s14, 0xfd469501);
-		FF(a, b, c, d, x[8], s11, 0x698098d8);
-		FF(d, a, b, c, x[9], s12, 0x8b44f7af);
-		FF(c, d, a, b, x[10], s13, 0xffff5bb1);
-		FF(b, c, d, a, x[11], s14, 0x895cd7be);
-		FF(a, b, c, d, x[12], s11, 0x6b901122);
-		FF(d, a, b, c, x[13], s12, 0xfd987193);
-		FF(c, d, a, b, x[14], s13, 0xa679438e);
-		FF(b, c, d, a, x[15], s14, 0x49b40821);
+		FF_NEON(a, b, c, d, x[0], s11, 0xd76aa478);
+		FF_NEON(d, a, b, c, x[1], s12, 0xe8c7b756);
+		FF_NEON(c, d, a, b, x[2], s13, 0x242070db);
+		FF_NEON(b, c, d, a, x[3], s14, 0xc1bdceee);
+		FF_NEON(a, b, c, d, x[4], s11, 0xf57c0faf);
+		FF_NEON(d, a, b, c, x[5], s12, 0x4787c62a);
+		FF_NEON(c, d, a, b, x[6], s13, 0xa8304613);
+		FF_NEON(b, c, d, a, x[7], s14, 0xfd469501);
+		FF_NEON(a, b, c, d, x[8], s11, 0x698098d8);
+		FF_NEON(d, a, b, c, x[9], s12, 0x8b44f7af);
+		FF_NEON(c, d, a, b, x[10], s13, 0xffff5bb1);
+		FF_NEON(b, c, d, a, x[11], s14, 0x895cd7be);
+		FF_NEON(a, b, c, d, x[12], s11, 0x6b901122);
+		FF_NEON(d, a, b, c, x[13], s12, 0xfd987193);
+		FF_NEON(c, d, a, b, x[14], s13, 0xa679438e);
+		FF_NEON(b, c, d, a, x[15], s14, 0x49b40821);
 
 		/* Round 2 */
-		GG(a, b, c, d, x[1], s21, 0xf61e2562);
-		GG(d, a, b, c, x[6], s22, 0xc040b340);
-		GG(c, d, a, b, x[11], s23, 0x265e5a51);
-		GG(b, c, d, a, x[0], s24, 0xe9b6c7aa);
-		GG(a, b, c, d, x[5], s21, 0xd62f105d);
-		GG(d, a, b, c, x[10], s22, 0x2441453);
-		GG(c, d, a, b, x[15], s23, 0xd8a1e681);
-		GG(b, c, d, a, x[4], s24, 0xe7d3fbc8);
-		GG(a, b, c, d, x[9], s21, 0x21e1cde6);
-		GG(d, a, b, c, x[14], s22, 0xc33707d6);
-		GG(c, d, a, b, x[3], s23, 0xf4d50d87);
-		GG(b, c, d, a, x[8], s24, 0x455a14ed);
-		GG(a, b, c, d, x[13], s21, 0xa9e3e905);
-		GG(d, a, b, c, x[2], s22, 0xfcefa3f8);
-		GG(c, d, a, b, x[7], s23, 0x676f02d9);
-		GG(b, c, d, a, x[12], s24, 0x8d2a4c8a);
+		GG_NEON(a, b, c, d, x[1], s21, 0xf61e2562);
+		GG_NEON(d, a, b, c, x[6], s22, 0xc040b340);
+		GG_NEON(c, d, a, b, x[11], s23, 0x265e5a51);
+		GG_NEON(b, c, d, a, x[0], s24, 0xe9b6c7aa);
+		GG_NEON(a, b, c, d, x[5], s21, 0xd62f105d);
+		GG_NEON(d, a, b, c, x[10], s22, 0x2441453);
+		GG_NEON(c, d, a, b, x[15], s23, 0xd8a1e681);
+		GG_NEON(b, c, d, a, x[4], s24, 0xe7d3fbc8);
+		GG_NEON(a, b, c, d, x[9], s21, 0x21e1cde6);
+		GG_NEON(d, a, b, c, x[14], s22, 0xc33707d6);
+		GG_NEON(c, d, a, b, x[3], s23, 0xf4d50d87);
+		GG_NEON(b, c, d, a, x[8], s24, 0x455a14ed);
+		GG_NEON(a, b, c, d, x[13], s21, 0xa9e3e905);
+		GG_NEON(d, a, b, c, x[2], s22, 0xfcefa3f8);
+		GG_NEON(c, d, a, b, x[7], s23, 0x676f02d9);
+		GG_NEON(b, c, d, a, x[12], s24, 0x8d2a4c8a);
 
 		/* Round 3 */
-		HH(a, b, c, d, x[5], s31, 0xfffa3942);
-		HH(d, a, b, c, x[8], s32, 0x8771f681);
-		HH(c, d, a, b, x[11], s33, 0x6d9d6122);
-		HH(b, c, d, a, x[14], s34, 0xfde5380c);
-		HH(a, b, c, d, x[1], s31, 0xa4beea44);
-		HH(d, a, b, c, x[4], s32, 0x4bdecfa9);
-		HH(c, d, a, b, x[7], s33, 0xf6bb4b60);
-		HH(b, c, d, a, x[10], s34, 0xbebfbc70);
-		HH(a, b, c, d, x[13], s31, 0x289b7ec6);
-		HH(d, a, b, c, x[0], s32, 0xeaa127fa);
-		HH(c, d, a, b, x[3], s33, 0xd4ef3085);
-		HH(b, c, d, a, x[6], s34, 0x4881d05);
-		HH(a, b, c, d, x[9], s31, 0xd9d4d039);
-		HH(d, a, b, c, x[12], s32, 0xe6db99e5);
-		HH(c, d, a, b, x[15], s33, 0x1fa27cf8);
-		HH(b, c, d, a, x[2], s34, 0xc4ac5665);
+		HH_NEON(a, b, c, d, x[5], s31, 0xfffa3942);
+		HH_NEON(d, a, b, c, x[8], s32, 0x8771f681);
+		HH_NEON(c, d, a, b, x[11], s33, 0x6d9d6122);
+		HH_NEON(b, c, d, a, x[14], s34, 0xfde5380c);
+		HH_NEON(a, b, c, d, x[1], s31, 0xa4beea44);
+		HH_NEON(d, a, b, c, x[4], s32, 0x4bdecfa9);
+		HH_NEON(c, d, a, b, x[7], s33, 0xf6bb4b60);
+		HH_NEON(b, c, d, a, x[10], s34, 0xbebfbc70);
+		HH_NEON(a, b, c, d, x[13], s31, 0x289b7ec6);
+		HH_NEON(d, a, b, c, x[0], s32, 0xeaa127fa);
+		HH_NEON(c, d, a, b, x[3], s33, 0xd4ef3085);
+		HH_NEON(b, c, d, a, x[6], s34, 0x4881d05);
+		HH_NEON(a, b, c, d, x[9], s31, 0xd9d4d039);
+		HH_NEON(d, a, b, c, x[12], s32, 0xe6db99e5);
+		HH_NEON(c, d, a, b, x[15], s33, 0x1fa27cf8);
+		HH_NEON(b, c, d, a, x[2], s34, 0xc4ac5665);
 
 		/* Round 4 */
-		II(a, b, c, d, x[0], s41, 0xf4292244);
-		II(d, a, b, c, x[7], s42, 0x432aff97);
-		II(c, d, a, b, x[14], s43, 0xab9423a7);
-		II(b, c, d, a, x[5], s44, 0xfc93a039);
-		II(a, b, c, d, x[12], s41, 0x655b59c3);
-		II(d, a, b, c, x[3], s42, 0x8f0ccc92);
-		II(c, d, a, b, x[10], s43, 0xffeff47d);
-		II(b, c, d, a, x[1], s44, 0x85845dd1);
-		II(a, b, c, d, x[8], s41, 0x6fa87e4f);
-		II(d, a, b, c, x[15], s42, 0xfe2ce6e0);
-		II(c, d, a, b, x[6], s43, 0xa3014314);
-		II(b, c, d, a, x[13], s44, 0x4e0811a1);
-		II(a, b, c, d, x[4], s41, 0xf7537e82);
-		II(d, a, b, c, x[11], s42, 0xbd3af235);
-		II(c, d, a, b, x[2], s43, 0x2ad7d2bb);
-		II(b, c, d, a, x[9], s44, 0xeb86d391);
+		II_NEON(a, b, c, d, x[0], s41, 0xf4292244);
+		II_NEON(d, a, b, c, x[7], s42, 0x432aff97);
+		II_NEON(c, d, a, b, x[14], s43, 0xab9423a7);
+		II_NEON(b, c, d, a, x[5], s44, 0xfc93a039);
+		II_NEON(a, b, c, d, x[12], s41, 0x655b59c3);
+		II_NEON(d, a, b, c, x[3], s42, 0x8f0ccc92);
+		II_NEON(c, d, a, b, x[10], s43, 0xffeff47d);
+		II_NEON(b, c, d, a, x[1], s44, 0x85845dd1);
+		II_NEON(a, b, c, d, x[8], s41, 0x6fa87e4f);
+		II_NEON(d, a, b, c, x[15], s42, 0xfe2ce6e0);
+		II_NEON(c, d, a, b, x[6], s43, 0xa3014314);
+		II_NEON(b, c, d, a, x[13], s44, 0x4e0811a1);
+		II_NEON(a, b, c, d, x[4], s41, 0xf7537e82);
+		II_NEON(d, a, b, c, x[11], s42, 0xbd3af235);
+		II_NEON(c, d, a, b, x[2], s43, 0x2ad7d2bb);
+		II_NEON(b, c, d, a, x[9], s44, 0xeb86d391);
 
-		state[0] += a;
-		state[1] += b;
-		state[2] += c;
-		state[3] += d;
+		// 累加当前块的计算结果到全局状态
+        A = vaddq_u32(A, a);
+        B = vaddq_u32(B, b);
+        C = vaddq_u32(C, c);
+        D = vaddq_u32(D, d);
 	}
 
+	// 将 NEON 向量状态存入输出二维数组 state_out
+    vst1q_u32(state_out[0], A);
+    vst1q_u32(state_out[1], B);
+    vst1q_u32(state_out[2], C);
+    vst1q_u32(state_out[3], D);
+
 	// 下面的处理，在理解上较为复杂
-	for (int i = 0; i < 4; i++)
-	{
-		uint32_t value = state[i];
-		state[i] = ((value & 0xff) << 24) |		 // 将最低字节移到最高位
-				   ((value & 0xff00) << 8) |	 // 将次低字节左移
-				   ((value & 0xff0000) >> 8) |	 // 将次高字节右移
-				   ((value & 0xff000000) >> 24); // 将最高字节移到最低位
+	// 后处理：对输出每个 32 位数进行字节顺序转换（大端转换）
+	for (int i = 0; i < 4; i++) {
+		uint32_t temp[4];
+		vst1q_u32(temp, state_out[i]);
+		for (int j = 0; j < 4; j++) {
+			uint32_t value = temp[j];
+			temp[j] = ((value & 0xff) << 24) |
+					((value & 0xff00) << 8) |
+					((value & 0xff0000) >> 8) |
+					((value & 0xff000000) >> 24);
+		}
+		memcpy(state_out[i], temp, sizeof(temp));
 	}
 
 	// 输出最终的hash结果
@@ -213,7 +237,11 @@ void MD5Hash(string input, bit32 *state)
 
 	// 释放动态分配的内存
 	// 实现SIMD并行算法的时候，也请记得及时回收内存！
-	delete[] paddedMessage;
-	delete[] messageLength;
+	// 释放动态分配的内存
+	for (int i = 0; i < 4; i++) {
+		delete[] paddedMessages[i];
+	}
+	// 这里 messageLength 最初为 4 个 int 存储，分配时可用 new int[4]
+	delete[] paddedLengths;
 }
 
